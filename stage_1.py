ed94b402132a712000f9f8f80f2f32b84d9af625
kobencry <ghostbencry@gmail.com>
2021-09-03 20:29:35 +0700

    mengubah isi dari file stage_1.py

--- stage_1.py
+++ stage_1.py
@@ -1,3 +1,11 @@
 class Main:
     def __init__(self):
         pass
+    
+    def __repr__(self):
+        return "hello world"
+    
+    def __str__(self):
+        retur self.__repr__()
+
+main = Main()
