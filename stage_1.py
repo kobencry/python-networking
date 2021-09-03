48ec2aff6416b528bc1efacf7c90bfb259778d31
kobencry <64555126+kobencry@users.noreply.github.com>
2021-09-03 20:29:00 +0700

    mengubah isi file stage_1.py

--- stage_1.py
+++ stage_1.py
@@ -1,3 +1,10 @@
 class Main:
     def __init__(self):
         pass
+    
+    def __repr__(self):
+        return "hello python"
+    
+    def __str__(self):
+        return self.__repr__()
+main = Main()
