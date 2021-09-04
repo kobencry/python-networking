8d44dd2a27df1969b8775a42fef9c24a19c77447
kobencry <64555126+kobencry@users.noreply.github.com>
2021-09-04 13:47:16 +0700

    update stage_1.py

--- stage_1.py
+++ stage_1.py
@@ -1,4 +1,10 @@
 class Main:
-    pass
-
+    def __init__(self):
+        print("hello world")
+     
+    def __repr__(self):
+        return "hello wrold")
+    
+    def __str__(self):
+        return self.__repr__()
 main = Main()
