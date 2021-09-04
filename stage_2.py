72135205bc5039b91c9bf1b49a93adb218d38e7d
kobencry <ghostbencry@gmail.com>
2021-09-04 13:05:58 +0700

    update stage_2.py

--- stage_2.py
+++ stage_2.py
@@ -1,5 +1,20 @@
+import sys
+
 def main():
   print("hello python")
 
+def on():
+    print("[+] status on")
+
+def off():
+    print("[-] status off")
+
+def passed():
+    sys.exit(1)
+
+
 if __name__=="__main__":
     main()
+    on()
+    off()
+    passed()
